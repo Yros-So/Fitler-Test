f216604b1d16e84be6910a29031fe0a4b62054b5
Yros-So <sowrosyo@gmail.com>
2026-07-28 19:52:14 +0200

    First Commit

--- backend/app/scraper/parsing.py
+++ backend/app/scraper/parsing.py
@@ -1,9 +1,19 @@
+# from bs4 import BeautifulSoup
+# from bs4.exceptions import FeatureNotFound
+
+
+# def parse_html(markup: str) -> BeautifulSoup:
+#     try:
+#         return BeautifulSoup(markup, "lxml")
+#     except FeatureNotFound:
+#         return BeautifulSoup(markup, "html.parser")
+
 from bs4 import BeautifulSoup
-from bs4.exceptions import FeatureNotFound
 
 
 def parse_html(markup: str) -> BeautifulSoup:
     try:
         return BeautifulSoup(markup, "lxml")
-    except FeatureNotFound:
+    except Exception:
         return BeautifulSoup(markup, "html.parser")
+
